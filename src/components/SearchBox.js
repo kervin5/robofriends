3fefa3470a6204705684f2351cd29138d88df6db
Kervin Vasquez <kervinlvh@gmail.com>
2019-05-06 23:52:22 -0700

    Completed PWA Robofriends

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const searchBox = ({searchField, searchChange}) => {
     return(
         <div className='pa2'>
-             <input type="search" placeholder="search robots" className="pa3 ba b--green bg-lightest-blue" onChange={searchChange}/>
+             <input aria-label='Search Robots' type="search" placeholder="search robots" className="pa3 ba b--green bg-lightest-blue" onChange={searchChange}/>
         </div>
 
     );
